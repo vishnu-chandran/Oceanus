ef6678fb9a1291b7efeb447423eda412e9f82414
vishnu <vishnuchandran19194@gmail.com>
2022-02-11 02:28:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function loginform()
+{
+	var email=document.getElementById("email");
+	var mailinfo=email.value;
+
+	var password=document.getElementById("password");
+	var Passwordinfo=password.value;
+
+   	alert(" Your email is "+ email + " " );
+}
+
+
